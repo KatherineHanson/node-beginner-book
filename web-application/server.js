7fbabf15222342f4aad1deb935822c07a49cc639
KatherineHanson <katherinemariehanson@gmail.com>
2017-09-24 17:09:33 -0700

    completed PNG display funcitonality

--- web-application/server.js
+++ web-application/server.js
@@ -7,22 +7,9 @@ var url = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var postData = '';
     var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + ' received.');
-
-    request.setEncoding('utf8');
-
-    request.addListener('data', function(postDataChunk) {
-      postData += postDataChunk;
-      // Adding console logging on every data chunk received =
-      // bad idea for production code, but makes sense to see what happens
-      console.log('Received POST data chunk "' + postDataChunk + '".');
-    });
-
-    request.addListener('end', function() {
-      route(handle, pathname, response, postData);
-    });
+    route(handle, pathname, response, request);
   }
 
   http.createServer(onRequest).listen(8888);
@@ -30,6 +17,37 @@ function start(route, handle) {
 }
 
 exports.start = start;
+
+// FINAL PRE-FORMIDABLE CODE FOR CREATING A SERVER
+// AND MESSAGE OUTPUT
+// var http = require('http');
+// var url = require('url');
+//
+// function start(route, handle) {
+//   function onRequest(request, response) {
+//     var postData = '';
+//     var pathname = url.parse(request.url).pathname;
+//     console.log('Request for ' + pathname + ' received.');
+//
+//     request.setEncoding('utf8');
+//
+//     request.addListener('data', function(postDataChunk) {
+//       postData += postDataChunk;
+//       // Adding console logging on every data chunk received =
+//       // bad idea for production code, but makes sense to see what happens
+//       console.log('Received POST data chunk "' + postDataChunk + '".');
+//     });
+//
+//     request.addListener('end', function() {
+//       route(handle, pathname, response, postData);
+//     });
+//   }
+//
+//   http.createServer(onRequest).listen(8888);
+//   console.log('Server has started.');
+// }
+//
+// exports.start = start;
 
 // FIRST SET OF CODE FOR CREATING A SERVER
 // AND MESSAGE OUTPUT
